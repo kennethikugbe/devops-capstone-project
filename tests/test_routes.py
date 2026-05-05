0c0bc736e3126c7832a37422fea179b2e4107808
kennethikugbe <kennethikugbe@gmail.com>
2026-05-05 08:03:48 -0400

    Implemented Delete Account endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -152,3 +152,16 @@ class TestAccountService(TestCase):
         """It should return 404 when updating non-existent Account"""
         resp = self.client.put(f"{BASE_URL}/999", json={"name": "Ghost"})
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(resp.data, b"")
+
+    def test_delete_account_not_found(self):
+        """It should return 204 even if Account not found"""
+        resp = self.client.delete(f"{BASE_URL}/999")
+        self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
